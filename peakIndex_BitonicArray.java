dd1ea99565a91aeffbc0d3f87f36b2ae780895b0
meghagupta0207 <meghaguptaprofile@gmail.com>
2026-07-26 22:45:06 +0530

    BitonicArray

---
+++ peakIndex_BitonicArray.java
@@ -0,0 +1,28 @@
+public class peakIndex_BitonicArray {
+
+    public static void main(String[] args){
+        int [] arr = {1,2,3,4,5,3,1};
+        System.out.println(peakIndex(arr));
+
+    }
+
+    static int peakIndex(int[] arr){
+
+        int start = 0;
+        int end = arr.length - 1;
+
+        while(start < end) { 
+            int mid = start + (end - start) / 2 ;
+
+            if(arr[mid] > arr[mid +1]){
+                end = mid; 
+            }
+            else {
+                start = mid + 1;
+            }
+            }
+            return start;
+        }
+    }
+    
+
