e422b5cb6c036a0a918c061affe2be94906b2ac7
meghagupta0207 <meghaguptaprofile@gmail.com>
2026-07-29 22:55:27 +0530

    Binary Search in 2D Matrix

---
+++ Binary_Search/binarysearch_2Dmatrix.java
@@ -0,0 +1,39 @@
+package Binary_Search;
+
+public class binarysearch_2Dmatrix {
+
+    public static void main(String[] args){
+
+        int[][] matrix = {
+            {1, 4, 7, 11, 15},
+            {2, 5, 8, 12, 19},
+            {3, 6, 9, 16, 22},
+            {10, 13, 14, 17, 24},
+            {18, 21, 23, 26, 30}
+        };
+        int target = 31;
+        int[] ans = search(matrix,target);
+        System.out.println("Target " + target + " found at: [" + ans[0] + ", " + ans[1] + "]");
+
+    }
+
+    static int[] search(int[][] matrix, int target){
+        int row = 0;
+        int col = matrix.length - 1;
+        while (row < matrix.length && col>=0){
+            if (matrix[row][col] == target){
+                return new int[]{row,col};
+            }
+            else if(matrix[row][col]< target){
+                row++;
+            }
+            else{
+                col--;
+            }
+            }
+            return new int[]{-1,-1};
+            }
+        }
+    
+    
+
